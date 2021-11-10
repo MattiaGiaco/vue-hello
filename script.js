bf5781bb25abc78a24560846cf99546282ba582d
mattiagiaco <martex781227@gmail.com>
2021-11-10 17:51:51 +0100

    bonus

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const app = new Vue({
   el: '#app',
   data: {
-    messaggio: 'Hello!'
+    messaggio: 'Hello!',
+    image: 'https://www.advisato.it/blog/wp-content/uploads/2017/05/michael-jordan-jumpman.jpg'
   },
   methods:{
     
